96dba67e8fa6f065b6d23a73d31c91e65e8999de
Walk-on-ice <66011531@kmitl.ac.th>
2024-07-26 11:44:18 +0700

    1-8

---
+++ Assignment4-5/calculator.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    return x + y
+def subtract(x,y):
+    return x - y
+def multiplay(x,y):
+    return x * y
+def divide(x,y):
+    return x / y
+
+print(add(5,3))
+print(subtract(5,3))
+print(multiplay(5,3))
+print(divide(5,3))
---
+++ Assignment6/main.py
@@ -0,0 +1,9 @@
+import string_operations as so
+
+sample_string = "hello world"
+
+print("Original: ", sample_string)
+print("Reverse: ",so.reverse_string(sample_string))
+print("Capitalize: ",so.capitalize_string(sample_string))
+print("Lowercase: ",so.lowercase_string(sample_string))
+print("Uppercase: ",so.uppercase_string(sample_string))
