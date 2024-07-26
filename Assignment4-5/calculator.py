a8b62bab99cb440ff1756cce81a69911a508530a
Walk-on-ice <66011531@kmitl.ac.th>
2024-07-26 12:14:22 +0700

    check again

--- Assignment4-5/calculator.py
+++ Assignment4-5/calculator.py
@@ -1,13 +1,13 @@
-def add(x,y):
+def add(x, y):
     return x + y
-def subtract(x,y):
+def subtract(x, y):
     return x - y
-def multiply(x,y):
+def multiply(x, y):
     return x * y
-def divide(x,y):
-    return x / y
+def divide(x, y):
+    return round(x / y, 2)
 
-print(add(5,3))
-print(subtract(5,3))
-print(multiply(5,3))
-print(divide(5,3))
+print(add(5, 3))
+print(subtract(5, 3))
+print(multiply(5, 3))
+print(divide(5, 3))
--- Assignment4-5/lambda_calculator.py
+++ Assignment4-5/lambda_calculator.py
@@ -1,7 +1,7 @@
 addition =  lambda x, y: x + y
 subtraction = lambda x, y: x - y
 multiplication = lambda x, y: x * y
-division = lambda x, y: x / y
+division = lambda x, y: round(x / y, 2)
 
 print(addition(5,3))
 print(subtraction(5,3))
